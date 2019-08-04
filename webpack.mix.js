1fcafbd380f9a887e9d18f122234241d35609174
Tatsuya Ichikawa <>
2019-08-04 23:55:08 +0900

    [Change] Change setting for path

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,6 @@ mix.browserSync('http://localhost:8000')
    .js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .version()
+   .options({
+      processCssUrls: false
+     });
